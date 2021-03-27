4b113294ed452f6da61f68671f3271a3b9b8b2f8
John Turner <johnturner4004@gmail.com>
2021-03-27 17:37:43 -0400

    finished comments on music-collection.js

--- assignment/scripts/music-collection.js
+++ assignment/scripts/music-collection.js
@@ -108,52 +108,65 @@ const showCollection = collection => {
 
 console.log('%c<<<<<<<<Testing showCollection()>>>>>>>>', 'color: black; background-color: orange');
 showCollection(collection);
+//this test is to check the error message for an invalid input
 showCollection(500);
 
 //-------------findByArtist()-------------//
-//array variable inserted for use with search().
+//array variable inserted for use with search(). If used on its own array is optional. Default array is collection
 const findByArtist = (artist, array) => {
   if (array === undefined) {
     array = collection;
   }
+  //array to store albums matching the artist argument. The following for loop will fill this array
   let matches = [];
   for (let i = 0; i < array.length; i++) {
     if (array[i].artist === artist) {
       matches.push(array[i]);
     }
   }
+  //message for when matches array is empty after searching the collection. Returns an empty array.
   if (matches.length === 0) {
     console.log(`%cThere are no albums by ${artist} in this collection`, `color: black; background-color: red`);
     return matches;
   }
+  //messages to log the matches array to the console formatted to make it easy to read.
   console.log(`This collection contains the following ${matches.length} albums by ${artist}:`);
+  //this for loop is to list all the tracks of an album in the console imediatly following the album they belong to
   for (let j = 0; j < matches.length; j++) {
     console.log(`${j + 1}. ${matches[j].title} by ${matches[j].artist}, published in the year ${matches[j].yearPublished}`);
   }
+  //returns the matches array
   return matches;
 };
 
 console.log('%c<<<<<<<<Testing findByArtist()>>>>>>>>', 'color: black; background-color: orange');
 findByArtist('Eminem');
 findByArtist('Unspoken');
+//this is to test error message
 findByArtist('Johnny Cash');
 
+
 //-------------findByTitle()-------------//
+//array is also optional when function is used by itself unless searching an array other than the default collection array
 const findByTitle = (title, array) => {
   if (array === undefined) {
     array = collection;
   }
+  //empty array to place albums with same title as the title argument followed by a for loop to fill that array
   let matches = [];
   for (let i = 0; i < array.length; i++) {
     if (array[i].title === title) {
       matches.push(array[i]);
     }
   }
+  //message for when there are no matching titles. Returns empty array
   if (matches.length === 0) {
     console.log(`%cThere are no albums titled ${title} in this collection`, `color: black; background-color: red`);
     return matches;
   }
+  //prints matches to the console. formatted to be easily read
   console.log(`This collection contains the following ${matches.length} albums titled ${title}:`);
+  //loop to list corresponting track list following the album they belong to
   for (let j = 0; j < matches.length; j++) {
     console.log(`${j + 1}. ${matches[j].title} by ${matches[j].artist}, published in the year ${matches[j].yearPublished}`);
   }
@@ -162,23 +175,31 @@ const findByTitle = (title, array) => {
 
 console.log('%c<<<<<<<<Testing findByTitle()>>>>>>>>', 'color: black; background-color: orange');
 findByTitle('Kamikaze');
+//this one to test error message
+findByTitle('Ring of Fire');
+
 
 //-------------findByYearPublished()-------------//
+//array argument is again optional. Default is collection array
 const findByYearPublished = (yearPublished, array) => {
   if (array === undefined) {
     array = collection;
   }
+  //empty array to store albums with matching yearPublished followed by for loop to search array
   let matches = [];
   for (let i = 0; i < array.length; i++) {
     if (array[i].yearPublished === yearPublished) {
       matches.push(array[i]);
     }
   }
+  //message for when there are no matches
   if (matches.length === 0) {
     console.log(`%cThere are no albums published in ${yearPublished} in this collection`, `color: black; background-color: red`);
     return matches;
   }
+  //message to log matches and make the log easy to read
   console.log(`This collection contains the following ${matches.length} published in ${yearPublished}:`);
+  //loop to pring corresponding tracks following the album they belong to
   for (let j = 0; j < matches.length; j++) {
     console.log(`${j + 1}. ${matches[j].title} by ${matches[j].artist}, published in the year ${matches[j].yearPublished}`);
   }
@@ -188,10 +209,12 @@ const findByYearPublished = (yearPublished, array) => {
 console.log('%c<<<<<<<<Testing findByYearPublished()>>>>>>>>', 'color: black; background-color: orange');
 findByYearPublished(2002);
 findByYearPublished(2015);
+//this one to test error message
 findByYearPublished(2020);
 
 
-console.log('%c<<<<<<<<Testing search()>>>>>>>>', 'color: black; background-color: orange');
+//functions to support search function.
+//the following three functions are condensed versions of the previous functions. The biggest difference is the console.log statements have been removed. I did not display individual tests for these functions as they were already tested above
 const findByArtistShort = (artist, array) => {
   if (array === undefined) {
     array = collection;
@@ -231,7 +254,8 @@ const findByYearPublishedShort = (yearPublished, array) => {
   return matches;
 };
 
-console.log('%c<<<<<<<<Testing search()>>>>>>>>', 'color: black; background-color: orange');
+
+//this function only has a short version as it was written after the search function. 
 const findByTrack = (track, array) => {
   if (array === undefined) {
     array = collection;
@@ -246,22 +270,30 @@ const findByTrack = (track, array) => {
   }
   return matches;
 };
+//logs to test findByTrack
 console.log(findByTrack('Without Me'));
 console.log(findByTrack('Chicken Fried'));
 
 
-
+//-------------search()-------------//
+//this is the big one. all arguments are optional. entering 0 arguments returns the collection array. 
 const search = (album, artist, yearPublished, trackName) => {
+  //one of the most awesome things this function does is printing results to the html page this identifies the output <p> element and sets it equal to the variable also named output
   let output = document.getElementById('output');
+  //this clears the webpage each time a new search is preformed
   output.innerHTML = '';
+  //first array is to store matches from the fisrt agrument that was not undefined. the second is to temporarily store results from following fingBy function so it can loop through the first then set the first equal to the second. any following findBy functions will reapeat this process. 
   let searchArr1 = [];
   let searchArr2 = [];
 
+  //checks for album agument and runs findByAlbum if one is present
   if (album !== undefined) {
     searchArr1 = findByTitleShort(album);
   }
 
+  //checks for artist argument
   if (artist !== undefined) {
+    //checks for album argument. if one is present this will run findByArtist using the searchArr1 and use the collection array if none is present
     if (album) {
       searchArr2 = findByArtistShort(artist, searchArr1);
       searchArr1 = searchArr2;
@@ -270,7 +302,9 @@ const search = (album, artist, yearPublished, trackName) => {
     }
   }
 
+  //checks if yearPublished argument is present
   if (yearPublished !== undefined) {
+    //checks if previous arguments are present. if so it runs findByYearPublished with the searchArr1 array, and if not it uses the colection array.
     if (album || artist) {
       searchArr2 = findByYearPublishedShort(yearPublished, searchArr1);
       searchArr1 = searchArr2;
@@ -279,7 +313,9 @@ const search = (album, artist, yearPublished, trackName) => {
     }
   }
 
+  //checks if trackName argument is present
   if (trackName !== undefined) {
+    //checks if any previous arguments were used. if so it runs findByTrack on searchArr1 and if not, ut uses coll
     if (album || artist || yearPublished) {
       searchArr2 = findByTrack(trackName, searchArr1);
       searchArr1 = searchArr2;
@@ -288,25 +324,35 @@ const search = (album, artist, yearPublished, trackName) => {
       console.log(searchArr1);
     }
   }
+
+  //chesks if there are no search arguments entered. sets searthArr1 equal to collection if non
   if (album === undefined && artist === undefined && yearPublished == undefined && trackName == undefined) {
     searchArr1 = collection;
   }
 
+  //checks if search had no results and prints log message if there is not one
   if (searchArr1.length === 0) {
     console.log(`%cThere are no albums or tracks matching search criteria`, `color: black; background-color: red`);
+    //prints same message to the webpage
     document.getElementById('output').innerHTML = `There are no albums or tracks matching search criteria`;
     return searchArr1;
   }
+
+//loop to log results to the console
   for (let j = 0; j < searchArr1.length; j++) {
     console.log(`%c${j + 1}. ${searchArr1[j].title} by ${searchArr1[j].artist}, published in the year ${searchArr1[j].yearPublished}`, `color: limegreen`);
+    //prints same message to the webpage
     output.innerHTML+= `<p class="title">` + `${j + 1}. ${searchArr1[j].title} by ${searchArr1[j].artist}, published in the year ${searchArr1[j].yearPublished}`;
+    //loop to print corresponding tracks to console log
     for (let k = 0; k < searchArr1[j].tracks.length; k++) {
       console.log(`\t${k + 1}. ${searchArr1[j].tracks[k].trackName}: ${searchArr1[j].tracks[k].duration}`);
+      //prints tracks to the webpacs
       output.innerHTML+= `${k + 1}. ${searchArr1[j].tracks[k].trackName}: ${searchArr1[j].tracks[k].duration}` + `<br>`;
     }
   }
 };
 
+//these were used to test search function. i commented them because it was printing things to the webpage that weren't searched for from the webpage. it works as expected
 /*console.log(1);
 search('Kamikaze');
 console.log(2);
@@ -328,33 +374,38 @@ search('', '', '', 'Chicken Fried');
 console.log(10);
 search();*/
 
+//loads the following when the page loads
 window.onload = function() {
-  console.log(1);
+  //identifies form element titles searchIn and sets it to the input variable
   const input = document.getElementById('searchIn');
-  console.log(2);
+  //runs the following code when submit button is clicked
   input.addEventListener('submit', event => {
-    console.log(3);
+    //prevents the page from reloading when submit button is pressed.
     event.preventDefault();
+    //retrieves input from album field
     let albumIn = document.getElementById('albumName').value;
     if (albumIn === '') {
       albumIn = undefined;
     }
-    console.log(albumIn);
+    //retrieves artist inbut field
     let artistIn = document.getElementById('artistName').value;
     console.log(artistIn);
     if (artistIn === '') {
       artistIn = undefined;
     }
+    //retrieves year published input field
     let yearPublishedIn;
+    //changes input type to number if it is a number
     if (isNaN(parseInt(document.getElementById('yearPublished').value)) === false) {
       yearPublishedIn = parseInt(document.getElementById('yearPublished').value);
     }
-    console.log(yearPublishedIn);
+    //retrives track name input field
     let trackIn = document.getElementById('track').value;
     console.log(trackIn);
     if (trackIn === '') {
       trackIn = undefined;
     }
+    //passes all input arguments to the search fungtion. 
     search(albumIn, artistIn, yearPublishedIn, trackIn);
 
   });
